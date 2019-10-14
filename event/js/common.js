2f76d9a2a38ca0482553c12e8dd1dc4f629ed899
Primerio <rio4poker@gmail.com>
2019-10-14 10:22:30 +0300

    d

--- event/js/common.js
+++ event/js/common.js
@@ -1,11 +1,41 @@
 $(document).ready(function() {
 
-$('.post_slider').slick({
+$('.top-slider').slick({
   dots: true,
   infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
+  speed: 300,
+  slidesToShow: 2,
+    'centerPadding': '60px',
+
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 
